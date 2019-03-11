9961cfe4d694e5cc9790811d3d0efac07b167cb1
hoangtv1899 <hoang.tran.1899@gmail.com>
2019-03-10 23:46:01 -0700

    python code can now connect and download files from Cyverse

--- CLMPostProc/Scripts/checkme.py
+++ CLMPostProc/Scripts/checkme.py
@@ -1,2 +1,26 @@
 import primes
-print(primes.__doc__)
+import os
+import sys
+import subprocess
+
+#print(primes.__doc__)
+#path to directory in cyverser
+files_path = '/iplant/home/shared/avra/CONUS_1.0/CC2019/Postproc_Test/Pressure/'
+
+#try to connect to cyverse server
+os.system('iinit')
+
+#check if connected and list files
+list_files = subprocess.check_output(['ils', files_path])
+
+list_files = [x.strip() for x in list_files.split('\n') if '.pfb' in x]
+
+if not list_files:
+	print 'please check the connection with cyverse'
+	sys.exit()
+
+for ii,fi in enumerate(list_files):
+	os.system('iget -K '+files_path+fi)
+	if ii == len(list_files)-1:
+		print 'done'
+	
